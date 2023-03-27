fe4a2aaba5f447c79fe8ac1732ee0897e669ab49
EnesT51 <99040669@mydavinci.nl>
2023-03-27 09:48:03 +0200

    HTML formulier verwerken - GET

---
+++ WelcomeGEt.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style/style.css">
+   <title>Form</title>
+</head>
+<body>
+
+   <div id="FormDiv">
+
+   Welkom <?php echo $_GET['fname']; ?><br>
+   Je email addres is: <?php echo $_GET['Email']; ?>
+
+   </div>
+
+</body>
+</html>
